ae1a54fa9ff850767ca440a66ca19125b0cfb04b
Magnus Malm <magnusmalm@gmail.com>
2016-09-27 23:12:09 +0200

    Emacs: View html mails with xwidgets
      - Some mails cannot be properly viewed in mu4e without xwidgets
        but now they can.
    
        Require that Emacs was built with --with-xwidgets

--- emacs/.emacs.d/config/mu4e-cfg.el
+++ emacs/.emacs.d/config/mu4e-cfg.el
@@ -144,4 +144,24 @@
 (add-hook 'mu4e-compose-mode-hook
           (lambda () (local-set-key (kbd "C-c C-w") #'my-mu4e-choose-signature)))
 
+;; works only for emacs with xwidget support
+
+(defun my-mu4e-action-view-with-xwidget (msg)
+  "View the body of the message inside xwidget-webkit."
+  (unless (fboundp 'xwidget-webkit-browse-url)
+    (mu4e-error "No xwidget support available"))
+  (let* ((html (mu4e-message-field msg :body-html))
+          (txt (mu4e-message-field msg :body-txt))
+          (tmpfile (format "%s%x.html" temporary-file-directory (random t))))
+    (unless (or html txt)
+      (mu4e-error "No body part for this message"))
+    (with-temp-buffer
+      ;; simplistic -- but note that it's only an example...
+      (insert (or html (concat "<pre>" txt "</pre>")))
+      (write-file tmpfile)
+      (xwidget-webkit-browse-url (concat "file://" tmpfile) t))))
+
+(add-to-list 'mu4e-view-actions
+	     '("xViewXWidget" . my-mu4e-action-view-with-xwidget) t)
+
 (provide 'mu4e-cfg)
